9b0bc716f3d4dae3e201789a2773dd74a76226e6
Albin <abbelsin@gmail.com>
2021-10-19 19:38:52 +0200

    wip

--- rust/day04/src/main.rs
+++ rust/day04/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::fs;
+
 //use std::io::BufRead;
 
 /**
@@ -12,31 +14,6 @@ use std::collections::HashSet;
  * pid (Passport ID)
  * cid (Country ID)
 */
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum PostE {
-    BirthYear,
-    IssueYear,
-    ExpirationYear,
-    Height,
-    HairColor,
-    EyeColor,
-    PassportID,
-    CountryID,
-}
-
-fn try_convert_post_e(input :&str) -> Option<PostE>{
-    match input {
-        "byr" => Some(PostE::BirthYear),
-        "iyr" => Some(PostE::IssueYear),
-        "eyr" => Some(PostE::ExpirationYear),
-        "hgt" => Some(PostE::Height),
-        "hcl" => Some(PostE::HairColor),
-        "ecl" => Some(PostE::EyeColor),
-        "pid" => Some(PostE::PassportID),
-        "cid" => Some(PostE::CountryID),
-        _ => None,
-    }
-}
 
 static TEST_INPUT: &str = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
@@ -52,49 +29,69 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in";
 
+const REQ_FIELDS: [&'static str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-fn parse_password(batch: &str) -> HashMap<PostE,&str> {
-    let b: Vec<&str> = batch.split(|c| c == ' ' || c == '\n').collect();
+fn parse_password(batch: &str) -> HashMap<&str,&str> {
+    let b: Vec<&str> = batch.split_ascii_whitespace().collect();
     let mut map = HashMap::new();
     for field in &b {
         let vals: Vec<&str> = field.split(":").collect();
-        map.entry(try_convert_post_e(vals[0]).unwrap()).or_insert(vals[1]);
-    }
-    println!("{:?}", b);
-    println!("{:?}", map);
-    let a = map.get(&PostE::PassportID);
-    
-
-    if let Some(f) = a {
-        let num: u32 = f.parse().unwrap();
-        println!("PID = {:?}", f);
-        println!("PID = {}", num);
         
+        if let Some(key) = vals.get(0) {
+            if let Some(val) = vals.get(1) {
+                map.entry(*key).or_insert(*val);
+            } else {
+                println!("Missing value for {:?}", key);
+            }
+        } else {
+            println!("Missing key");
+        }
     }
     return map;
     
 }
+fn validate(pwd_map: &HashMap<&str,&str>) -> bool {
+    return REQ_FIELDS.iter().all(|field| pwd_map.contains_key(field));
+}
 
-const REQ_FIELDS: [&'static str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+fn validate_better(pwd_map: &HashMap<&str,&str>) -> bool {
+    let has_valid_fields = REQ_FIELDS.iter().all(|field| pwd_map.contains_key(field));
+    let byr = pwd_map.get("byr").unwrap();
+    let iyr = pwd_map.get("iyr").unwrap();
+    let eyr = pwd_map.get("eyr").unwrap();
+    let hgt = pwd_map.get("hgt").unwrap();
+    let hcl = pwd_map.get("hcl").unwrap();
+    let ecl = pwd_map.get("ecl").unwrap();
+    let pid = pwd_map.get("pid").unwrap();
+    let cid = pwd_map.get("cid").unwrap();
+    byr >=1920 && byr <=2002
+    
+}
 
-const REQQ_FIELDS: [&'static str; 3] = ["Norway", "Denmark", "Iceland"];
 
-fn main2() {
+
+fn main() {
     println!("~~~~ AoC 2020 day04 ~~~~");
-    let inputs: Vec<&str> = TEST_INPUT.split("\n\n").collect();
-    parse_password(inputs[1]);
-
-    //let timber_resources: HashSet<&str, i32> = [("Norway", 100), ("Denmark", 50), ("Iceland", 10)].iter().cloned().collect();
+    let input_string = include_str!("../input.txt");
+    let inputs: Vec<&str> = input_string.split("\r\n\r\n").collect();
+    
+    let passwords: Vec<HashMap<&str,&str>> = inputs.iter().map(|pwd| parse_password(pwd)).collect();
+    
+    let n = passwords.iter().count();
+    println!("{} passwords!", n);
+    println!("{} valid passwords!", passwords.iter().filter(|pwd| validate(pwd)).count());
 
     //println!("{}",REQQ_FIELDS.iter().all(|item| timber_resources.contains(item)));
 }
 
 // sick solution from internet
-pub fn main() {
+#[test]
+pub fn main2() {
+    let input_string = include_str!("../input.txt");
     println!(
         "{}",
-        TEST_INPUT
-            .split("\n\n")
+        input_string
+            .split("\r\n\r\n")
             .map(|fields| fields
                 .split_ascii_whitespace()
                 .map(|field| field.split(':').next().unwrap())
